7c19dd6da4960e60657d40b987c86d4d8f1fca2c
FATİH TOĞUŞ <f6820023@std.yildiz.edu.tr>
2021-02-06 23:24:00 +0300

    06.02 - 2. Ders

---
+++ Business/Abstract/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    // Kategori ile ilgili dış dünyaya neleri servis etmek istiyorsak buraya yazarız!
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        Category GetById(int categoryId);
+    }
+}
--- DataAccess/Concrete/EntitiyFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntitiyFramework/EfOrderDal.cs
@@ -1,18 +1,14 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntitiyFramework
 {
-    //NuGet
-    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
+    class EfOrderDal : EfEntityRepositoryBase<Order, NorthwindContext>, IOrderDal
     {
-        
+
     }
 }
